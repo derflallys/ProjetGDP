8bf7b8cf80920d8f8bc7bd67c0ff81c73f54674c
derflallys <a.aboubacar.sylla@gmail.com>
2017-03-26 02:56:35 +0100

    Modification hearder avec dropdown

--- core/header.php
+++ core/header.php
@@ -3,11 +3,11 @@
             <div class="nav-wrapper container">
                 <a id="logo-container" href="../index.php" class="brand-logo"><img class="circle responsive-img" src="../core/img/logo.jpg" alt="Logo"></a>
                 <ul class="right hide-on-med-and-down">
-                    <li><a href="#"><i class="material-icons left">face</i>Mon Compte</a></li>
+                    <li><a href="#" class="dropdown-button" data-activates="moncompte"><i class="material-icons left">face</i>Mon Compte</a></li>
                     <li><a href="#"><i class="material-icons left">exit_to_app</i>Deconnexion</a></li>
                 </ul>
                 <ul id="moncompte" class="dropdown-content">
-                    <li><a href="#!"><i class="material-icons">info</i>Mes Informations</a></li>
+                    <li><a href="#!">Mes Informations</a></li>
                     <li><a href="#!">Mes Publications</a></li>
                 </ul>
                 <ul id="nav-mobile" class="side-nav">
