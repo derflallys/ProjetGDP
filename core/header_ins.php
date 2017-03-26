1668d4f7093c3451d751d1f0215eed77e12e9f3e
Adama90210 <edelweiss90210@gmail.com>
2017-03-26 03:12:20 +0000

    ajout header pour inscription

---
+++ core/header_ins.php
@@ -0,0 +1,22 @@
+<div class="navbar-fixed">
+    <nav class="white" role="navigation">
+            <div class="nav-wrapper container">
+                <a id="logo-container" href="../index.php" class="brand-logo"><img class="circle responsive-img" src="../core/img/logo.jpg" alt="Logo"></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a class="dropdown-button" data-activates="moncompte" href="#"><i class="material-icons left">face</i>Mon Compte</a></li>
+                    <li><a href="#"><i class="material-icons left">exit_to_app</i>Deconnexion</a></li>
+                </ul>
+                <ul id="moncompte" class="dropdown-content">
+                    <li><a href="#!">Mes Informations</a></li>
+                    <li><a href="#!">Mes Publications</a></li>
+                </ul>
+                <ul id="nav-mobile" class="side-nav">
+
+                        <li><a href="#"><i class="material-icons left">face</i>Mon Compte</a></li>
+                        <li><a href="#"><i class="material-icons left">exit_to_app</i>Deconnexion</a></li>
+
+                </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+            </div>
+    </nav>
+</div>
