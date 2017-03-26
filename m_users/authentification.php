4d0b3c0d4ca3da9bb3611636e6affaca16174f74
issamboup <issamboup2015@gmail.com>
2017-03-26 02:49:36 +0100

    Apres login et mise à jour inscription

--- m_users/authentification.php
+++ m_users/authentification.php
@@ -1,11 +1,47 @@
 <?php
+
+
 session_start();
-var_dump($_SESSION);
-$id=isset($_SESSION['id'])? $_SESDION['id'] :'' ;
-$password=isset($_SESSION['password'])? $_SESDION['password'] :'' ;
-$email=isset($_SESSION['email'])? $_SESDION['email'] :'' ;
 
 
 
+include "../core/connexionBd.php";
 
+
+$connexion = connexionBd();
+$bd=BD;
+
+if (isset($_POST['connect'])) 
+{
+	$sql="SELECT * from $bd.donateurs where email=:email AND password=:password";
+	$resultat=$connexion->prepare($sql);
+	$mdp=hash('sha256',$_POST['password']);
+	echo $mdp;
+
+	 $resultat->execute(array('email'=>$_POST['email'] ,'password'=>$mdp ));
+	$res = $resultat->fetchAll(PDO::FETCH_ASSOC);
+
+
+		if (!empty($res)) {
+
+            foreach ($res as $key => $value) {
+
+                $_SESSION['id'] = $value['id'];
+                $_SESSION['email'] = $value['email'];
+                $_SESSION['password'] = $value['password'];
+                
+            }
+            echo "bon";
+            header('Location:../m_pub/affichePublication.php');
+		}
+		else {
+
+			$_SESSION['donateurs']['error']="Login ou Mot de passe Incorrect";
+			echo "mauvais login";
+		}
+}
+else
+{
+	echo "mauvais";
+}
 ?>
